8a75e8ec09ebd5c0b57f90733c3b1d05d2491aa8
arpa646 <sadikahosen50@gmail.com>
2024-03-06 21:16:25 +0600

    menuItem set

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -3,7 +3,9 @@ import MenuItem from "../../../Shared/MenuItem/MenuItem";
 import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
 import UseMenu from "../../../Hooks/UseMenu";
 import Category from "./../Category/Category";
-
+//custom hooks just a normal function we use
+// we use custom hooks for reuse code 
+//and return nessecery thing that we can use many where
 const PopularMenu = () => {
   const [menu, loader] = UseMenu();
   // const [menu, setMenu] = useState([]);
@@ -17,6 +19,7 @@ const PopularMenu = () => {
   // }, []);
 
   const popularMenu = menu?.filter((each) => each.category == "popular");
+  
   console.log(popularMenu);
 
   return (
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import Cover from "../../Shared/Cover/Cover";
-import img1 from '../../assets/menu/menu-bg.jpg'
+import img1 from "../../assets/menu/menu-bg.jpg";
+import UseMenu from "../../Hooks/UseMenu";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import menuImg from '../../assets/menu/menu-bg.jpg'
+import soupImg from '../../assets/menu/soup-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import MenuCategory from "../Menucategory/MenuCategory";
 const Menu = () => {
+  const [menu, loader] = UseMenu([]);
+ 
+  const dessert = menu?.filter((each) => each.category == "popular");
+  const pizza = menu?.filter((each) => each.category == "pizza");
+  const salad = menu?.filter((each) => each.category == "salad");
+  const soup = menu?.filter((each) => each.category == "soup");
+  const offered = menu?.filter((each) => each.category == "offered");
+  
   return (
     <div>
       <Helmet>
         <title>coffeshop|| Menu</title>
       </Helmet>
-       <Cover img={img1} title="Our menu"  ></Cover>
+      <Cover img={menuImg} title="our menu"></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items  */}
+            <MenuCategory items={dessert} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
     </div>
   );
 };
